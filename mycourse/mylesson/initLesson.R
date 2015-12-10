d1d171df2d567815046ad6c696909a67443ed8a5
soyung <soyung@users.noreply.github.com>
2015-12-10 03:47:37 -0500

    pew

---
+++ mycourse/mylesson/initLesson.R
@@ -0,0 +1,25 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+twix <- "WILLIE: I thought you said it was a Twix.
+
+GEORGE: Oh, it was. But he claimed it was a 5th Avenue bar.
+
+WILLIE: Maybe it was.
+
+GEOR: Oh, no, no. Twix is the only candy with the cookie crunch.
+
+WILLIE: What about the $100,000 bar?
+
+G: No. Rice and caramel.
+
+WILLIE: Nougat?
+
+GG: No.
+
+WILLIE: Positive?
+
+GeorgE: Please."
+
