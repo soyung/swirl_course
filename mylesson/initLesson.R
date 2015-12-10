8c88291dbd4e70d4531965249e7a0090e5a00e10
soyung <soyung@users.noreply.github.com>
2015-12-10 04:01:02 -0500

    init

--- mylesson/initLesson.R
+++ mylesson/initLesson.R
@@ -3,6 +3,13 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
+.path_to_file <<- function(course, lesson, file_name){
+  file.path(path.package('swirl'), 'Courses', course,
+            lesson, file_name)
+}
+
+
+
 twix <- "WILLIE: I thought you said it was a Twix.
 
 GEORGE: Oh, it was. But he claimed it was a 5th Avenue bar.
